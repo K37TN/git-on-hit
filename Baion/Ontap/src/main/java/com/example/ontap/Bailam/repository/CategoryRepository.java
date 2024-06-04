1d7be4332f22dffa571ee41a5343e9a775561912
Nguyễn Đức Mạnh <93751413+K37TN@users.noreply.github.com>
2024-06-04 10:45:51 +0700

    Create CategoryRepository.java

---
+++ Baion/Ontap/src/main/java/com/example/ontap/Bailam/repository/CategoryRepository.java
@@ -0,0 +1,7 @@
+package com.example.ontap.Bailam.repository;
+
+import com.example.ontap.Bailam.entity.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoryRepository extends JpaRepository<Category,String> {
+}
