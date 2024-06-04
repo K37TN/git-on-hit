b5a10a948cd2bbec292ea26423937d013cddc33c
Nguyễn Đức Mạnh <93751413+K37TN@users.noreply.github.com>
2024-06-04 08:15:14 +0700

    B

---
+++ Baion/Ontap/src/test/java/com/example/ontap/OntapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ontap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OntapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
